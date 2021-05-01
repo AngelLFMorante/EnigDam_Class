c34dc7d92439e601ae9bf1ee0b4fde4c367092a0
Angel <angellfmorante@gmail.com>
2021-05-01 13:14:09 +0200

    #12 create view fragment game and directory game witch class

---
+++ app/src/main/java/pm/iesvives/enigdam_class/Game/NavHost.java
@@ -0,0 +1,14 @@
+package pm.iesvives.enigdam_class.Game;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import pm.iesvives.enigdam_class.R;
+
+public class NavHost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_host);
+    }
+}
